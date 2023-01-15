93650190ca0747e8810c4f7c64b37f93bd8221c8
Raymond Kwok <rmwkwok@gmail.com>
2023-01-15 15:31:13 +0800

    first commit

---
+++ conv2d_transpose_numpy.py
@@ -0,0 +1,249 @@
+import numpy as np
+
+def numpy_conv2d(inputs, kernels, strides, padding, return_kernel=False):
+    '''
+    Convolute `inputs` with `kernels`.
+    
+    Args:
+        inputs: A `numpy.ndarray` of shape (number of samples, number of
+            channels, height, width)
+        kernels: A `numpy.ndarray` of shape (number of kernels, 
+            number of channels, height, width). The number of channels 
+            of `kernels` must match with that of `inputs`.
+        strides: A positive `int`.
+        padding: A non-negative `int`.
+        return_kernel: A `bool`. If `True`, return the unrolled kernel
+            and the convoluted outputs, otherwise, return only the 
+            convoluted outputs.
+        
+    Returns:
+        outputs: A `numpy.ndarray`. The convoluted outputs.
+    '''
+    _inputs = _padding(inputs, padding, 'normal')
+    outputs_shape = compute_outputs_shape(_inputs, kernels, strides, 'normal')
+    _kernels = _unroll_kernel(_inputs, kernels, outputs_shape, 'normal')
+    
+    _inputs = _flatten(_inputs)
+    outputs = np.tensordot(_inputs, _kernels, ((1, 2), (0, 1)))
+    outputs = outputs.transpose(0,2,1).reshape(outputs_shape)
+    outputs = _stride(outputs, strides, 'normal')
+    
+    if return_kernel:
+        return (outputs, _kernels)
+    
+    return outputs
+
+def numpy_conv2d_transpose(inputs, kernels, strides, outputs_shape, padding, 
+                           return_kernel=False):
+    '''
+    Transposed convolute `inputs` with `kernels`.
+    
+    Args:
+        inputs: A `numpy.ndarray` of shape (number of samples, number of
+            channels, height, width)
+        kernels: A `numpy.ndarray` of shape (number of channels, number 
+            of kernels, height, width). The number of channels of
+            `kernels` must match with that of `inputs`.
+        strides: A positive `int`.
+        outputs_shape: A `numpy.ndarray` of shape (number of samples, 
+            number of channels, height, width). It is computed by 
+            `compute_outputs_shape()`.
+        padding: A non-negative `int`.
+        return_kernel: A `bool`. If `True`, return the unrolled kernel
+            and the transposed-convoluted outputs, otherwise, return only
+            the transposed-convoluted outputs.
+        
+    Returns:
+        outputs: A `numpy.ndarray`. The convoluted outputs.
+    '''
+    _inputs = _stride(inputs, strides, 'transposed')
+    _kernels = _unroll_kernel(_inputs, kernels, outputs_shape, 'transposed')
+
+    _inputs = _flatten(_inputs)
+    outputs = np.tensordot(_inputs, _kernels, ((1, 2), (0, 1)))
+    outputs = outputs.transpose(0,2,1).reshape(outputs_shape)
+    outputs = _padding(outputs, padding, 'transposed')
+    
+    if return_kernel:
+        return (outputs, _kernels)
+    
+    return outputs
+
+#################
+## utils
+#################
+
+def _padding(inputs, padding, mode):
+    '''
+    Pad (`mode=='normal'`) or crop (`mode=='transposed'`). 
+    
+    Args:
+        inputs: A `numpy.ndarray` of shape (number of samples, number of
+            channels, height, width).
+        padding: A non-negative `int`.
+        mode: A `str`. Either 'normal' or 'tranposed'.
+        
+    Returns:
+        outputs: A `numpy.ndarray`. Padded or cropped `inputs`.
+    '''
+    p = padding
+    m, c, h, w = inputs.shape
+    
+    if mode == 'normal': # do padding
+        outputs = np.zeros((m, c, h+2*p, w+2*p), dtype=np.float32)
+        outputs[:,:,p:h+p,p:w+p] = inputs
+        
+    elif mode == 'transposed': # do cropping
+        outputs = inputs[:,:,p:h-p,p:w-p]
+        
+    return outputs
+
+def _flatten(inputs):
+    '''
+    Flatten the axes for height and width.
+    
+    Args:
+        inputs: A `numpy.ndarray` of shape (number of samples, number of
+            channels, height, width).
+        
+    Returns:
+        outputs: A `numpy.ndarray`. Flattened `inputs`.
+    '''
+    m, c, h, w = inputs.shape
+    return inputs.reshape((m, c, -1))
+
+def _stride(inputs, strides, mode):
+    '''
+    If `mode=='normal'`, keep an element of `inputs`every `strides` step
+    along both height and width axes. This shrinks `inputs`. If 
+    `mode='transposed'`, add `strides` zeros between every element of
+    `inputs` along both height and width axes. This expands `inputs`.
+    
+    Args:
+        inputs: A `numpy.ndarray` of shape (number of samples, number of
+            channels, height, width).
+        strides: A positive `int`.
+        mode: A `str`. Either 'normal' or 'tranposed'.
+        
+    Returns:
+        outputs: A `numpy.ndarray`. Expanded or shrinked.
+    '''
+    s = strides
+    m, c, h, w = inputs.shape
+    
+    if mode == 'normal': # get every other N rows/columns
+        outputs = inputs[:,:,::s,::s]
+        
+    elif mode == 'transposed': # add N rows/columns of 0 between two rows/columns
+        _h = h + (h - 1) * (s - 1)
+        _w = w + (w - 1) * (s - 1)
+        outputs = np.zeros((m,c,_h,_w), dtype=np.float32)
+        outputs[:,:,::s,::s] = inputs
+        
+    return outputs
+
+def _unroll_kernel(inputs, kernels, outputs_shape, mode):
+    '''
+    Unroll `kernels`
+    
+    Args:
+        inputs: A `numpy.ndarray` of shape (number of samples, number of
+            channels, height, width)
+        kernels: A `numpy.ndarray`. If `mode=='normal'`, it has the 
+            shape (number of kernels, number of channels, height, 
+            width). If `mode=='transposed'`, it has the shape of (
+            number of channels, number of kernels, height, width). The 
+            number of channels of `kernels` must match with that of 
+            `inputs`.
+        outputs_shape: A `numpy.ndarray` of shape (number of samples, 
+            number of channels, height, width). It is computed by 
+            `compute_outputs_shape()`
+        mode: A `str`. Either 'normal' or 'tranposed'
+        
+    Returns:
+        outputs: A `numpy.ndarray`. Unrolled kernel.
+    '''
+#     __, __, oh, ow = outputs_shape
+#     __, ic, ih, iw = inputs.shape
+#     kn, kc, kh, kw = kernels.shape
+    
+#     if mode == 'normal':
+#         assert ic == kc
+#         _kernel = np.zeros((kc, ih, iw, kn), dtype=np.float32)
+#         _kernel[:,:kh,:kw,:] = kernels.transpose(1,2,3,0)
+#         _kernel = _kernel.reshape(kc, -1, kn)
+#         outputs = np.stack([
+#             np.roll(_kernel, iw * x + y, axis=1) for x in range(oh) for y in range(ow)
+#         ], axis=2)
+        
+#     elif mode == 'transposed':
+#         assert ic == kn
+#         _kernel = np.zeros((kn, oh, ow, kc), dtype=np.float32)
+#         _kernel[:,:kh,:kw,:] = kernels.transpose(0,2,3,1)
+#         _kernel = _kernel.reshape(kn, -1, kc)
+#         outputs = np.stack([
+#             np.roll(_kernel, ow * x + y, axis=1) for x in range(ih) for y in range(iw)
+#         ], axis=1)
+    
+    inputs_shape = inputs.shape
+
+    if mode == 'transposed':
+        inputs_shape, outputs_shape = outputs_shape, inputs_shape
+        kernels = kernels.transpose(1,0,2,3)    
+
+    __, ic, ih, iw = inputs_shape
+    __, oc, oh, ow = outputs_shape
+    kn, kc, kh, kw = kernels.shape
+    
+    _kernel = np.zeros((kc, ih, iw, kn), dtype=np.float32)
+    _kernel[:,:kh,:kw,:] = kernels.transpose(1,2,3,0)
+    _kernel = _kernel.reshape(kc, -1, kn)
+
+    r = np.arange(oh*ow) // ow * (kw - 1)
+    y, x = np.ogrid[:ih*iw,:oh*ow]
+    idx = (y - (x + r)) % (ih*iw)
+
+    if mode == 'transposed':
+        idx = idx.T
+
+    outputs = _kernel[:,idx,:]
+        
+    return outputs
+
+def compute_outputs_shape(inputs, kernels, strides, mode):
+    '''
+    Compute the output shape of the normal/transposed convolution.
+    
+    Args:
+        inputs: A `numpy.ndarray` of shape (number of samples, number of
+            channels, height, width)
+        kernels: A `numpy.ndarray`. If `mode=='normal'`, it has the 
+            shape (number of kernels, number of channels, height, 
+            width). If `mode=='transposed'`, it has the shape of (
+            number of channels, number of kernels, height, width). The 
+            number of channels of `kernels` must match with that of 
+            `inputs`.
+        strides: A positive `int`.
+        mode: A `str`. Either 'normal' or 'tranposed'
+        
+    Returns:
+        outputs: A `numpy.ndarray`. Output shape.
+    '''
+    im, ic, ih, iw = inputs.shape
+    kn, kc, kh, kw = kernels.shape
+    
+    if mode == 'normal':
+        outputs_shape = (
+            im, 
+            kn,
+            ih - kh + 1, 
+            iw - kw + 1)
+    
+    elif mode == 'transposed':
+        outputs_shape = (
+            im, 
+            kc,
+            (ih - 1) * strides + kh, 
+            (iw - 1) * strides + kw)
+
+    return outputs_shape
---
+++ tests.py
@@ -0,0 +1,174 @@
+import torch
+import numpy as np
+import tensorflow as tf
+from conv2d_transpose_numpy import numpy_conv2d, numpy_conv2d_transpose, \
+                                   compute_outputs_shape
+
+def test_normal_convolution(inputs, kernels, strides, print_arrays=False):
+    '''
+    Test convolution, and compare results with Tensorflow's and Torch's 
+    implementation.
+    
+    Args:
+        inputs: A `numpy.ndarray` of shape (number of samples, number of
+            channels, height, width)
+        kernels: A `numpy.ndarray` of shape (number of kernels, number
+            of channels, height, width). The number of channels of
+            `kernels` must match with that of `inputs`.
+        strides: A positive `int` 
+        print_arrays: A `bool`. If `True`, print the convoluted arrays
+            by implementation
+    '''
+    
+    assert inputs.shape[1] == kernels.shape[1]
+    
+    print('Normal Convolution tests\n')
+    
+    # Tensorflow
+    inputs_tf = inputs.transpose(0,2,3,1)
+    kernels_tf = kernels.transpose(2,3,1,0)
+    outputs_tensorflow = tf.nn.conv2d(
+        inputs_tf, kernels_tf, strides=strides, 
+        padding='VALID')
+    outputs_tensorflow = outputs_tensorflow.numpy()
+    outputs_tensorflow = outputs_tensorflow.transpose(0,3,1,2)
+
+    # Torch
+    inputs_torch = torch.from_numpy(inputs)
+    kernels_torch = torch.from_numpy(kernels)
+    outputs_torch = torch.nn.functional.conv2d(
+        inputs_torch, kernels_torch, stride=strides, padding='valid')
+    outputs_torch = outputs_torch.numpy()
+
+    # Numpy
+    outputs_numpy = numpy_conv2d(inputs, kernels, strides, padding=0)
+    
+    print('AllClose Check: TensorFlow vs Numpy:', 
+          'Passed' if np.allclose(outputs_tensorflow, outputs_numpy)\
+              else 'Failed')
+    
+    print('AllClose Check: Torch vs Numpy:',
+          'Passed' if np.allclose(outputs_torch, outputs_numpy)\
+              else 'Failed')
+    
+    print('\nOutput Shapes:')
+    print('Numpy:', outputs_numpy.shape)
+    print('PyTorch:', outputs_torch.shape)
+    print('Tensorflow:', outputs_tensorflow.shape)
+    
+    if print_arrays:
+        print('\n\nTensorflow')
+        print(outputs_tensorflow)
+        print('\n\nTorch')
+        print(outputs_torch)
+        print('\n\nNumpy')
+        print(outputs_numpy)
+        
+def test_transposed_convolution(inputs, kernels, strides, padding, 
+                                print_arrays=False):
+    '''
+    Test Transposed Convolution, and compare results with Tensorflow's
+    and Torch's implementation.
+    
+    Args:
+        inputs: A `numpy.ndarray` of shape (number of samples, number of 
+            channels, height, width)
+        kernels: A `numpy.ndarray` of shape (number of channels, number 
+            of kernels, height, width). The number of channels of
+            `kernels` must match with that of `inputs`.
+        strides: A positive `int` 
+        padding: A non-negative `int` for Torch and Numpy 
+            implementations only. Tensorflow does not use padding
+        print_arrays: A `bool`. If `True`, print the convolution-
+            transposed arrays by implementation
+    '''
+    
+    assert inputs.shape[1] == kernels.shape[0]
+    
+    print('Transposed Convolution tests\n')
+    
+    outputs_shape = compute_outputs_shape(inputs, kernels, strides, 
+                                         mode='transposed')
+    
+    # Tensorflow
+    inputs_tf = inputs.transpose(0,2,3,1)
+    kernels_tf = kernels.transpose(2,3,1,0)
+    outputs_shape_tf = (
+        outputs_shape[0], outputs_shape[2], 
+        outputs_shape[3], outputs_shape[1])
+    
+    # outputs_tensorflow = tf.nn.conv2d_transpose(
+    #     inputs_tf, kernels_tf, strides=strides, output_shape=outputs_shape_tf, 
+    #     padding='VALID')
+    # outputs_tensorflow = outputs_tensorflow.numpy()
+    # outputs_tensorflow = outputs_tensorflow.transpose(0,3,1,2)
+    
+    kwargs = dict(
+        filters=kernels_tf.shape[2],
+        kernel_size=kernels_tf.shape[:2],
+        strides=strides,
+        use_bias=False,
+    )
+    
+    outputs_tensorflow1 = tf.keras.layers.Conv2DTranspose(
+        padding='valid',
+        output_padding=None,
+        **kwargs
+    )(inputs_tf).numpy().transpose(0,3,1,2)
+    
+    outputs_tensorflow2 = tf.keras.layers.Conv2DTranspose(
+        padding='same',
+        output_padding=None,
+        **kwargs
+    )(inputs_tf).numpy().transpose(0,3,1,2)
+    
+    outputs_tensorflow3 = tf.keras.layers.Conv2DTranspose(
+        padding='valid',
+        output_padding=0,
+        **kwargs
+    )(inputs_tf).numpy().transpose(0,3,1,2)
+    
+    outputs_tensorflow4 = tf.keras.layers.Conv2DTranspose(
+        padding='same',
+        output_padding=0,
+        **kwargs
+    )(inputs_tf).numpy().transpose(0,3,1,2)
+    
+    # Torch
+    inputs_torch = torch.from_numpy(inputs)
+    kernels_torch = torch.from_numpy(kernels)
+    outputs_torch = torch.nn.functional.conv_transpose2d(
+        inputs_torch, kernels_torch, stride=strides, padding=padding)
+    outputs_torch = outputs_torch.numpy()
+
+    # Numpy
+    outputs_numpy = numpy_conv2d_transpose(
+        inputs, kernels, strides, outputs_shape, padding)
+    
+    print('AllClose Check: Torch vs Numpy:',
+          'Passed' if np.allclose(outputs_torch, outputs_numpy)\
+              else 'Failed')
+    
+    print('\nOutput Shapes:')
+    print('Numpy:', outputs_numpy.shape)
+    print('PyTorch:', outputs_torch.shape)
+    
+    print('\nVarious Tensorflow Output Shapes:')
+    print('padding=\'valid\' output_padding=None', outputs_tensorflow1.shape)
+    print('padding=\'same\'  output_padding=None', outputs_tensorflow2.shape)
+    print('padding=\'valid\' output_padding=0   ', outputs_tensorflow3.shape)
+    print('padding=\'same\'  output_padding=0   ', outputs_tensorflow4.shape)
+    
+    if print_arrays:
+        print('\n\nTorch')
+        print(outputs_torch)
+        print('\n\nNumpy')
+        print(outputs_numpy)
+        print('\n\nTensorflow: padding=\'valid\' output_padding=None')
+        print(outputs_tensorflow1)
+        print('\n\nTensorflow: padding=\'same\'  output_padding=None')
+        print(outputs_tensorflow2)
+        print('\n\nTensorflow: padding=\'valid\' output_padding=0   ')
+        print(outputs_tensorflow3)
+        print('\n\nTensorflow: padding=\'same\'  output_padding=0   ')
+        print(outputs_tensorflow4)
